cc32e34de1651b12e825192d476261b7f80721e4
mayookh <mayookhbino77@gmail.com>
2025-01-12 11:41:18 +0530

    base url changed

--- src/service.ts/AdminService.ts
+++ src/service.ts/AdminService.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL ='https://kyc-backend-lblx.vercel.app/api/admin'
+const API_URL ='https://kyc-backend-jade.vercel.app/api/admin'
 axios.defaults.withCredentials=true
 const apiservice=axios.create({
     baseURL:API_URL,
--- src/service.ts/UserService.ts
+++ src/service.ts/UserService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { kycSubmit, LoginFormData } from "../Interfaces/forms";    
-const API_URL ='https://kyc-backend-lblx.vercel.app/api/user'
+const API_URL ='https://kyc-backend-jade.vercel.app/api/user'
 axios.defaults.withCredentials=true
 const apiservice=axios.create({
     baseURL:API_URL,
